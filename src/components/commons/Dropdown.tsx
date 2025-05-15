0c57b5e5e4d0a4a3e12ac3e635bf673fd269065f
whdjh <whdjh98@naver.com>
2025-05-15 11:43:51 +0900

    feat: 드롭다운 메뉴부 구현완료

--- src/components/commons/Dropdown.tsx
+++ src/components/commons/Dropdown.tsx
@@ -23,22 +23,21 @@ export default function DropdownMenuDropdown() {
       <div className="relative bg-white">
         <button
           onClick={toggleDropdown}
-          className="flex items-center justify-between gap-[10px] rounded-xl border border-gray-100 px-[12px] py-[8px] text-black"
+          className="flex items-center justify-between gap-[10px] rounded-xl border border-gray-100 px-[12px] py-[8px] text-gray-800"
         >
           {/*TODO: 이미지 관련 수정 필요 */}
           <img src={Sort.src} alt="정렬" width={20} height={20} />
           <span>{selectedDropdownMenu}</span>
         </button>
 
-        {/* Dropdown Menu */}
         {isOpen && (
-          <div>
+          <div className="absolute gap-[10px] rounded-xl border border-gray-100 bg-white text-gray-800">
             {DropdownMenus.map((DropdownMenu) => (
               <div
                 key={DropdownMenu}
                 onClick={() => selectDropdownMenu(DropdownMenu)}
+                className="cursor-pointer rounded-xl px-[12px] py-[8px] hover:bg-orange-100"
               >
-                {selectedDropdownMenu === DropdownMenu && <span>✓</span>}
                 <span>{DropdownMenu}</span>
               </div>
             ))}
