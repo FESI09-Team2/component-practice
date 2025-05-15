56c46845cecc38f3e18243c561c23d0546052b47
강은우 <80903685+eunwoo66@users.noreply.github.com>
2025-05-15 10:30:43 +0900

    chore: tailwind css 설정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,17 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    rules: {
+      'react/function-component-definition': [
+        'error',
+        {
+          namedComponents: 'function-declaration',
+        },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
