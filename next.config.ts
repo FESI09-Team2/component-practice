56e76b148d745758fdac268f0200636550aa2744
young-02 <taeyoung917@gmail.com>
2025-05-16 17:15:47 +0900

    fix:svg import 형식 변경

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js',
+      },
+    },
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
