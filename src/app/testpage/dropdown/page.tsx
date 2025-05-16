42e3d76201f281dcda633c0f97dd7ca2a02f10c1
whdjh <99352927+whdjh@users.noreply.github.com>
2025-05-16 16:34:21 +0900

    feat: [컴포넌트] Dropdown (#6)
    * fix: ''수정 및 npm설치
    
    * feat: 정렬 이미지 추가
    
    * feat: 드롭다운 버튼부 구현완료
    
    * feat: 드롭다운 메뉴부 구현완료
    
    * feat: 핸들링 구분
    
    * feat: interface추가 및 정렬기능 함수 추가
    
    * chore: 초기셋팅으로 인한 커밋
    
    * chore: main의 컴포넌트 testpage로 이동
    
    * refactor: interface 파일로 분리
    
    * refactor: sort기능 hook으로 분리
    
    * fix: dropdown 메뉴 props로 전달 받을 수 있음
    
    * refactor: 이미지도 Props로 받을 수 있음
    
    * chore: 경로 @public으로 변경
    
    * feat: useSort 훅 테스트 추가
    
    * feat: dropdown 테스트 추가
    
    * fix: px -> rem
    
    * feat: 모바일일때 텍스트 숨김
    
    * fix: 기본 이미지와 변경되는 이미지 위치 변경
    
    * feat: 드롭다운 외부 클릭시 닫힘
    
    * chore: 드롭다운 외부 클릭시 닫힘 훅으로 분리
    
    * chore: 이벤트 핸들러 이름 변경
    
    * chore: 오타 수정 및 컴포넌트  이름 수정
    
    * chore: 이미지 경로 이동
    
    * feat: 외부요소 감지 테스트 코드 작성
    
    * chore: type 폴더 각 파일로 이동
    
    * chore: w-auto, h-auto의 주석 추가
    
    * fix: 웹팩 적용해서 이미지 컴포넌트 처럼 사용
    
    ---------
    
    Co-authored-by: whdjh <whdjh98@naver.com>

--- src/app/testpage/dropdown/page.tsx
+++ src/app/testpage/dropdown/page.tsx
@@ -1,5 +1,31 @@
 'use client';
 
+import { useState } from 'react';
+import Dropdown from '@/components/commons/Dropdown';
+import useSortedData from '@/hooks/useSortByOption';
+import ArrowDown from '@/assets/common/ic_arrowdown.svg';
+
 export default function Home() {
-  return <div>dropdown의 테스트 페이지입니다.</div>;
+  const [sortOption, setSortOption] = useState('최신 순');
+  const sortedData = useSortedData(sortOption);
+
+  return (
+    <>
+      <Dropdown
+        onSelect={setSortOption}
+        menuOptions={['최신 순', '리뷰 높은 순', '참여 인원 순']} // 여기서 정렬 옵션을 설정
+        customIcon={<ArrowDown />} // 지정하지 않으면 기본 아이콘 사용
+      />
+      <div className="mt-4 space-y-2">
+        {sortedData.map((item) => (
+          <div key={item.id} className="rounded-lg border px-4 py-2 shadow-sm">
+            <p className="font-semibold">{item.title}</p>
+            <p>리뷰: {item.review}</p>
+            <p>참여 인원: {item.participants}</p>
+            <p>작성일: {item.createdAt}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
---
+++ src/components/commons/Dropdown.tsx
@@ -0,0 +1,71 @@
+'use client';
+
+import { ReactNode, useRef, useState } from 'react';
+import DefaultSortIcon from '@/assets/common/ic_sort.svg';
+import { useClickOutside } from '@/hooks/useClickOutside';
+
+interface DropdownProps {
+  onSelect: (selectedDropdownMenu: string) => void;
+  menuOptions: string[];
+  customIcon?: ReactNode;
+}
+
+export default function Dropdown({
+  onSelect,
+  menuOptions,
+  customIcon,
+}: DropdownProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedDropdownMenu, setSelectedDropdownMenu] = useState('최신 순');
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  useClickOutside(dropdownRef, () => setIsOpen(false));
+
+  const handleDropdownMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleSelect = (DropdownMenu: string) => {
+    setSelectedDropdownMenu(DropdownMenu);
+    setIsOpen(false);
+    onSelect(DropdownMenu);
+  };
+
+  return (
+    // TODO: 글자나 시안 확정됨에 따라 w-auto랑 h-auto 수정
+    <div className="h-auto w-auto" ref={dropdownRef}>
+      <div className="relative bg-white">
+        <button
+          onClick={handleDropdownMenu}
+          className="flex items-center justify-between gap-[0.625rem] rounded-xl border border-gray-100 px-[0.75rem] py-[0.5rem] text-gray-800"
+        >
+          {customIcon ? (
+            <>
+              {customIcon}
+              <span>{selectedDropdownMenu}</span>
+            </>
+          ) : (
+            <>
+              <span className="hidden sm:inline">{selectedDropdownMenu}</span>
+              <DefaultSortIcon />
+            </>
+          )}
+        </button>
+
+        {isOpen && (
+          <div className="absolute gap-[0.625rem] rounded-xl border border-gray-100 bg-white text-gray-800">
+            {menuOptions.map((option) => (
+              <div
+                key={option}
+                onClick={() => handleSelect(option)}
+                className="cursor-pointer rounded-xl px-[0.75rem] py-[0.5rem] hover:bg-orange-100"
+              >
+                <span>{option}</span>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
