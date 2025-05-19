4e1ccab7c2226af261fe4f28cb4e858e3b5bd755
whdjh <whdjh98@naver.com>
2025-05-19 14:37:33 +0900

    feat: useClickOutside CSR동작 가능함을 알리는 주석 추가

--- src/hooks/useClickOutside.tsx
+++ src/hooks/useClickOutside.tsx
@@ -1,5 +1,9 @@
 import { useEffect } from 'react';
 
+/**
+ * useClickOutside hook은 브라우저 환경(클라이언트 사이드)에서만 동작
+ * SSR 환경에서는 document가 없기 때문에 사용 시 주의하세요.
+ */
 export function useClickOutside(
   ref: React.RefObject<HTMLElement | null>,
   onClickOutside: () => void,
