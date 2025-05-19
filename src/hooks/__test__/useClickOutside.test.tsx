90b6a9146eab9403e29f26f4e60e25b01c904396
whdjh <whdjh98@naver.com>
2025-05-19 14:34:53 +0900

    chore: test 코드 주석 제거

--- src/hooks/__test__/useClickOutside.test.tsx
+++ src/hooks/__test__/useClickOutside.test.tsx
@@ -2,10 +2,8 @@ import React, { useRef } from 'react';
 import { render, fireEvent, cleanup } from '@testing-library/react';
 import { useClickOutside } from '../useClickOutside';
 
-// 각 테스트 후 DOM 정리
 afterEach(cleanup);
 
-// 테스트용 컴포넌트 정의
 function TestComponent({ onClickOutside }: { onClickOutside: () => void }) {
   const ref = useRef<HTMLDivElement | null>(null);
 
@@ -13,12 +11,10 @@ function TestComponent({ onClickOutside }: { onClickOutside: () => void }) {
 
   return (
     <div>
-      {/* 클릭 감지를 위한 내부 요소 */}
       <div ref={ref} data-testid="inside">
         Inside Element
       </div>
 
-      {/* 외부 클릭을 시뮬레이션할 요소 */}
       <div data-testid="outside">Outside Element</div>
     </div>
   );
