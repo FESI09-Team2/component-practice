ed170127a09bec68d889777ddd39431695990ccc
강은우 <80903685+euoonw@users.noreply.github.com>
2025-05-19 17:49:41 +0900

    feat: [컴포넌트] Button (#7)
    * feat: 커스텀 컬러 테마 설정 추가
    
    * feat: 공통 버튼  컴포넌트 추가
    
    * feat: 공통 버튼  컴포넌트 사용 예시 테스트페이지 작성
    
    * refactor: px 단위를 rem 단위로 변경
    
    * test: 공통 버튼 컴포넌트 테스트 코드 추가
    
    * fix: 버튼 타입 명시
    
    * fix: 컬러변수 삭제, 테일윈드 기본색상 팔레트 사용으로 변경
    
    * feat: 커서 스타일 추가
    
    ---------
    
    Co-authored-by: 강은우 <80903685+eunwoo66@users.noreply.github.com>

---
+++ src/components/commons/Button.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { clsx } from 'clsx';
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+  variant?: 'solid' | 'outline';
+  size?: 'large' | 'small';
+  disabled?: boolean;
+  className?: string;
+}
+
+export default function Button({
+  children,
+  variant = 'solid',
+  size = 'large',
+  disabled = false,
+  className,
+  ...props
+}: ButtonProps) {
+  const buttonClasses = clsx(
+    'h-[2.75rem] rounded-[0.75rem] flex items-center justify-center text-[1rem] font-semibold cursor-pointer disabled:cursor-not-allowed',
+    {
+      'bg-orange-600 hover:bg-orange-700 active:bg-orange-800  text-white':
+        variant === 'solid' && !disabled,
+      'bg-white border border-solid border-orange-600 text-orange-600 hover:border-orange-500 hover:text-orange-500 active:border-orange-700 active:text-orange-700':
+        variant === 'outline' && !disabled,
+      'bg-gray-400  text-white': variant === 'solid' && disabled,
+      'bg-white border border-solid border-gray-400 text-gray-400':
+        variant === 'outline' && disabled,
+    },
+    {
+      'w-[20.7rem]': size === 'large',
+      'w-[7.5rem]': size === 'small',
+    },
+    className,
+  );
+
+  return (
+    <button
+      type="button"
+      className={buttonClasses}
+      disabled={disabled}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
